3f6c315e48a8bf4ba2ffc64533e9384fb86c9063
NOAA Education and Outreach <JPessutti@NOAAs-iMac.local>
2017-05-02 15:42:32 -0400

    Initial Commit

---
+++ Sound Board/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sound Board
+//
+//  Created by NOAA Education and Outreach on 5/2/17.
+//  Copyright © 2017 PMG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
